c60eefc2f87654c52b95fa264d5b379d6e674c02
Your name here <gunkiddedy@gmail.com>
2020-07-14 22:44:20 +0700

    fix bug 2

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,13 +9,27 @@
     <h3>About Us</h3>
     <hr class="featurette-divider">
     <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">{{ $about->title }}</h2>
-            <p class="lead">{{ $about->description }}</p>
-        </div>
-        <div class="col-md-5">
-            <img src="{{ asset('storage/'.$about->image) }}" alt="images" class="card-img">
-        </div>
+        @if (!empty($about))
+            <div class="col-md-7">
+                <h2 class="featurette-heading">{{ $about->title }}</h2>
+                <p class="lead">{{ $about->description }}</p>
+            </div>
+            <div class="col-md-5">
+                <img src="{{ asset('storage/'.$about->image) }}" alt="images" class="card-img">
+            </div>
+        @else 
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Lorem ipsum</h2>
+                <p class="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Sapiente rem illo saepe id blanditiis at laudantium magnam. 
+                    Sequi, asperiores dolorem.
+                </p>
+            </div>
+            <div class="col-md-5">
+                <img src="{{ asset('storage/images/'.'default-avatar.png') }}" alt="images" class="card-img">
+            </div>
+        @endif
     </div>
 </div>
 
