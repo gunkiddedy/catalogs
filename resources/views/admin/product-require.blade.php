618457e4436be5a47a12e5cc83ffcd90ae02cdb8
Your name here <gunkiddedy@gmail.com>
2020-08-09 18:08:03 +0700

    too many revision dude :-)

--- resources/views/admin/product-require.blade.php
+++ resources/views/admin/product-require.blade.php
@@ -52,11 +52,11 @@
                                     </td>
                                     <td>
                                         <div class="d-flex justify-content-start">
-                                            {{-- <a href="{{ route('product-require.edit', $item->id) }}" class="text-white btn btn-sm btn-primary mr-1">
-                                                <i class="fa fa-pencil"></i>
-                                            </a> --}}
                                             <a href="{{ route('product-require.detail', $item->id) }}" class="text-white btn btn-sm btn-primary mr-1">
                                                 <i class="fa fa-eye"></i>
+                                            </a>
+                                            <a href="mailto:{{ \App\User::find($item->user_id)->email }}" class="btn btn-primary btn-sm text-white">
+                                                <i class="fa fa-envelope"></i> {{ \App\User::find($item->user_id)->email }}
                                             </a>
                                         </div>
                                     </td>
