87fa48677d54c91941a4883761cd44b6e6054ed4
Your name here <gunkiddedy@gmail.com>
2020-08-09 23:03:42 +0700

    too many revision dude :-)

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,5 +1,4 @@
 <div id="product" class="col-md-2 col-sm-12 sidebar_admin_mobile mb-4">
-    <span class="badge badge-success text-white mb-1">Hallo, {{ Auth::user()->name }}!</span>
-
+    {{-- <span class="badge badge-success text-white mb-1">Hallo, {{ Auth::user()->name }}!</span> --}}
     <admin-sidebar></admin-sidebar>
 </div>
